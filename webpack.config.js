2fcb6b55196f27b5dbb7ee582d70e3e603bcb372
Robin <robin@robinmiau.de>
2025-11-13 02:21:57 +0100

    refactor: update webpack configuration for clarity and consistency

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
+//@ts-check
+'use strict';
+
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-/**@type {import('webpack').Configuration}*/
-const config = {
-    target: 'node', // VS Code extensions run in a Node.js-context
-    mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
 
-    entry: './src/extension.ts', // the entry point of this extension
+/** @type WebpackConfig */
+const extensionConfig = {
+    target: 'node',
+    mode: 'none',
+    entry: './src/extension.ts',
     output: {
         path: path.resolve(__dirname, 'out'),
         filename: 'extension.js',
         libraryTarget: 'commonjs2'
     },
     externals: {
-        vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded
+        vscode: 'commonjs vscode'
     },
     resolve: {
         extensions: ['.ts', '.js']
@@ -46,8 +50,8 @@ const config = {
     ],
     devtool: 'nosources-source-map',
     infrastructureLogging: {
-        level: "log", // enables logging required for problem matchers
-    },
+        level: 'log'
+    }
 };
 
-module.exports = config;
+module.exports = [extensionConfig];
